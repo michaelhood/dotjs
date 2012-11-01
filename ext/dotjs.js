7b5675b4b49476f19ade5d47b63d5e5b78a56880
Justin Hileman <justin@justinhileman.info>
2012-11-01 11:18:14 -0700

    Use a domready callback, don't eval JS.

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,14 @@
-$.ajax({
-  url: 'http://localhost:3131/'+window.location.hostname.replace('www.','')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
-  }
-})
+
+jQuery(function($) {
+  var path   = 'http://localhost:3131/'+window.location.hostname.replace('www.', '');
+  var script = document.createElement('script');
+  var handle = function() {
+    console.log('no dotjs server found at localhost:3131');
+  };
+
+  script.src     = path + '.js';
+  script.async   = true;
+  script.onerror = handle;
+
+  document.head.appendChild(script);
+});
