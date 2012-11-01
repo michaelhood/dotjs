c6c6794b063758d419a5cc47a134a3f8a21e4654
Justin Hileman <justin@justinhileman.info>
2012-11-01 11:18:37 -0700

    Add .css file support to daemon, dotjs.js

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -2,6 +2,7 @@
 jQuery(function($) {
   var path   = 'http://localhost:3131/'+window.location.hostname.replace('www.', '');
   var script = document.createElement('script');
+  var style  = document.createElement('link');
   var handle = function() {
     console.log('no dotjs server found at localhost:3131');
   };
@@ -10,5 +11,10 @@ jQuery(function($) {
   script.async   = true;
   script.onerror = handle;
 
+  style.href     = path + '.css';
+  style.rel      = 'stylesheet';
+  style.onerror  = handle;
+
   document.head.appendChild(script);
+  document.head.appendChild(style);
 });
